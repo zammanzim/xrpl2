a409ae9872ff9a5a55a664439cccc4f56ab4fd53
zammanzim <nizzcuy@gmail.com>
2025-10-24 13:21:23 +0700

    jumv3

--- script.js
+++ script.js
@@ -17,4 +17,44 @@ toggle.addEventListener('click', () => {
     const visible = gallery.style.display === 'flex';
     gallery.style.display = visible ? 'none' : 'flex';
     toggle.classList.toggle('active');
+})
+function searchPage() {
+    const input = document.getElementById('searchInput');
+    const query = document.getElementById('searchInput').value.trim().toLowerCase();
+    const message = document.getElementById('searchMessage');
+
+    message.textContent = "";
+
+    const data = [
+        { keyword: "1", url: "bahasajepang.html#1" },
+        { keyword: "2", url: "pjok.html#2" },
+        { keyword: "3", url: "informatika.html#3" },
+        { keyword: "4", url: "pabp.html#4" },
+        { keyword: "5", url: "pp.html#5" },
+        { keyword: "6", url: "matematika.html#6" },
+        { keyword: "7", url: "dpr2.html#7" }
+    ];
+
+    const result = data.find(item => item.keyword === query);
+
+    if (result) {
+        window.location.href = result.url;
+    } else {
+        input.value = "";
+
+        const oldPlaceholder = input.placeholder;
+        input.classList.add('error');
+        input.placeholder = "gaada woi";
+
+        setTimeout(() => {
+            input.placeholder = oldPlaceholder;
+            input.classList.remove('error');
+        }, 2500);
+    }
+}
+
+document.getElementById('searchInput').addEventListener('keydown', function (e) {
+    if (e.key === 'Enter') {
+        searchPage();
+    }
 });
